e7554673df5c17cc1c81b2588cc166bc9cb9dac6
Bividib <masterbivas@gmail.com>
2023-10-04 11:54:36 +0100

    Add max length to name in Thing model

--- things/models.py
+++ things/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MaxValueValidator,MinValueValidator
 
 class Thing(models.Model):
-    name = models.CharField(max_length=15,unique=True,primary_key=True,blank=False)
+    name = models.CharField(max_length=30,unique=True,primary_key=True,blank=False)
     description = models.CharField(max_length=120,unique=False,blank=True,)
     quantity = models.IntegerField(validators=[MaxValueValidator(100),MinValueValidator(0)])
 
